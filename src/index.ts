a695bab5d6725d3c176f40f84c89bfac16472b9e
Swaroop Dhungana <52006830+notori0usss@users.noreply.github.com>
2023-05-16 15:57:42 +0545

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 
 const app = express();
-app.use('/', (req, res) => {
+app.use(express.json());
+
+app.get('/', (req, res):void => {
     res.send('<h1>Thanks for using this repo! Make sure to give it a star</h1>');
 });
 
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
-});
+app.listen(8000, () => console.log("server started on http://localhost:8000/"));
